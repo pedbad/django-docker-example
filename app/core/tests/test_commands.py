e4c31f7a4bf514d77092e747c9ffb1ca3b3a166a
pedbad <pb357@cam.ac.uk>
2023-06-20 12:47:16 +0100

    fixed a typo in testcommands.py

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -11,10 +11,10 @@ from unittest.mock import patch
 
 @patch('core.management.commands.wait_for_db.Command.check')  # Mocking the check method of the wait_for_db command
 class CommandTests(SimpleTestCase):
-    """ Test wait_for_db command. """
+    """Test wait_for_db command."""
 
     def test_wait_for_db_ready(self, patched_check):
-        """ Test waiting for database when the database is ready. """
+        """Test waiting for database when the database is ready."""
         
         # Mocking the return value of the check method to indicate database readiness
         patched_check.return_value = True  
@@ -23,12 +23,12 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         # Asserting that the check method was called with the correct arguments
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
-        """Test waiting for database when getting OperationalError. """
+        """Test waiting for database when getting OperationalError."""
 
         # Configure the side effect of the patched check method
         patched_check.side_effect = [psycopg2Error] * 3 + [OperationalError] * 4 + [True]
@@ -40,4 +40,4 @@ class CommandTests(SimpleTestCase):
         self.assertEqual(patched_check.call_count, 8)
 
         # Assert that the check method was called with the correct arguments
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
